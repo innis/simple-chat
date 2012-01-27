2fd18ec7fe935b1ffb6392f8620cb694ed12a6ca
inniskun <inniskun@gmail.com>
2012-01-27 09:44:21 +0900

    init chat

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = require("express").createServer(),
+    io = require("socket.io").listen(app);
+
+// 실제 heroku에서는 PORT를 사용. 현재 Cloud9ide에선 C9_PORT사용
+var port = process.env.C9_PORT;
+if (!port){
+    port = process.env.PORT;
+}
+
+app.listen(port);
+
+app.get("/", function(req, res){
+    res.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on("connection", function(socket){
+    socket.on("chat", function(data){
+        console.log("receivedata = " + data);
+        socket.broadcast.emit("chat", data);
+    });
+});

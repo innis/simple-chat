ced5ed16b5322a9772cb968d1355e774ff3a07fc
inniskun <inniskun@gmail.com>
2012-01-27 15:57:55 +0900

    rere

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (wsocketio3) is running..');
-}).listen(14033);
+var app = require("express").createServer(),
+    io = require("socket.io").listen(app);
+
+// 실제 heroku에서는 PORT를 사용. 현재 Cloud9ide에선 C9_PORT사용
+var port = process.env.app_port;
+//if (!port){
+//    port = process.env.PORT;
+//}
+
+app.listen(port);
+
+app.get("/", function(req, res){
+    res.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on("connection", function(socket){
+    socket.on("chat", function(data){
+        console.log("receivedata = " + data);
+        socket.broadcast.emit("chat", data);
+    });
+});

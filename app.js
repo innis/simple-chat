5be4c10e1673841d8d0bf1822d9d34a228ecf29a
inniskun <inniskun@gmail.com>
2012-01-27 14:59:20 +0900

    change port

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var app = require("express").createServer(),
     io = require("socket.io").listen(app);
 
 // 실제 heroku에서는 PORT를 사용. 현재 Cloud9ide에선 C9_PORT사용
-var port = process.env.C9_PORT;
-if (!port){
-    port = process.env.PORT;
-}
+var port = process.env.app_port;
+//if (!port){
+//    port = process.env.PORT;
+//}
 
 app.listen(port);
 
